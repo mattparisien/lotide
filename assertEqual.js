2db78825d9a37e93d2bd12cb1a3f471532ad0c30
Matthew Parisien <matthewparisien4@gmail.com>
2021-07-19 22:51:23 +0000

    Implementing a custom function which compares two values and returns a message confirming whether they are equal or differ

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👑👑👑 Assertion Passed: " + "[" + actual + "]" + " === " + "[" + expected + "]");
+  } else if (actual !== expected) {
+    console.log("💩💩💩 Assertion Failed: " + "[" + actual + "]" + " !== " + "[" + expected + "]");
+  }
+};
+
+// TESTING CODE
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual("Javascript", "Node"));
+console.log(assertEqual(10, 25));
+console.log(assertEqual(2, 2));
+
+
