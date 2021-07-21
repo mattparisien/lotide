f2d7bb8e3940323e263903629604eee562ec2d39
Matthew Parisien <matthewparisien4@gmail.com>
2021-07-21 13:44:19 +0000

    Creating a function that flattens 2D array into one array.

---
+++ flatten.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arrOne, arrTwo) {
+  let str = "";
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] === arrTwo[i]) {
+      str += arrOne[i];
+    }
+  }
+  let compareStr = arrOne.join("");
+  if (compareStr === str) {
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log("The two arrays match!");
+  } else {
+    console.log("The two arrays do not match.");
+  }
+};
+
+const flatten = function(array) {
+  let flattenedArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        flattenedArr.push(array[i][j]);
+      }
+    } else {
+      flattenedArr.push(array[i]);
+    }
+  }
+  return flattenedArr;
+};
+
+//Test Case 1: Testing the flatten function
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([["Coffee"], "tea", ["cookies"]]));
+console.log(flatten([["Bananas"], "Cherries", ["Lemons", "Avocados"]]));
+
+//Test Case 2: Using the assertArraysEqual to test out 
+console.log(assertArraysEqual(flatten([1, 2, 3, [4, 5], 6]), [1, 2, 3, 4, 5, 6]));
+
