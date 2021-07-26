b9411afcfd3eb7bb315c1bb65be8de41115c18c9
Matthew Parisien <matthewparisien4@gmail.com>
2021-07-26 13:30:21 +0000

    Creating a function that counts the occurence of each letter in a given string by returning an object.

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👑👑👑 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const obj = {};
+  for (letter of string) {
+    if (obj[letter]) {
+      obj[letter] += 1;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+return obj;
+};
+
+
+//TEST CASES
+
+const result1 = countLetters("matthew");
+
+console.log(result1);
+
+const result2 = {
+  m: 1,
+  a: 1,
+  t: 2,
+  h: 1,
+  e: 1,
+  w: 1,
+};
+
+console.log(result2);
+
+console.log(assertEqual(result1, result2));
