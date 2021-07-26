9f6b34bbc7777d84e3355572195dec1c3d805da1
Matthew Parisien <matthewparisien4@gmail.com>
2021-07-26 20:03:21 +0000

    Creating a function which takes in two objects as arguments, and uses the eqObjects function in its body to print out an assertion message, based on eqObjects return value.

---
+++ assertObjectsEqual.js
@@ -0,0 +1,36 @@
+const eqObjects = function(object1, object2) {
+
+  const object1Keys = Object.keys(object1); 
+  const object2Keys = Object.keys(object2);
+
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  } else {
+    for (let key of object1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])){
+        console.log(object1[key], object2[key]);
+        return eqArrays(object1[key], object2[key])
+      } else if (object1[key] !== object2[key]) {
+          return false;
+      }
+    }  
+  }
+  return true;
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  const result = eqObjects(actual, expected);
+
+  if (result === true) {
+    console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+
+};
+
+//TEST CASES
+
+console.log(assertObjectsEqual({Matt: 1, Caroline: 2, Denis: 3}, {Matt: 1, Caroline: 2, Denis: 3}));
