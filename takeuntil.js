f335aa7ed067b2d17169a2ab41bbd3ed0d53a746
Matthew Parisien <matthewparisien4@gmail.com>
2021-07-27 20:53:22 +0000

    Building a function which iterates through an array and returns all elements up until a condition specified by a callback

---
+++ takeuntil.js
@@ -0,0 +1,24 @@
+const takeUntil = function(array, callback) {
+
+const finalArr = [];
+
+  for (item of array) {
+    if (!callback(item)) {
+      finalArr.push(item);
+    } else {
+      return finalArr;
+    }
+  }
+  
+};
+
+//TEST CASES
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
