0f5d51010c8eb2151c7f0bdf3d9875dd00dd81f0
Matthew Parisien <matthewparisien4@gmail.com>
2021-07-28 12:31:39 +0000

    Implementing our own map function which takes in an array and a callback and returns a modified array which is modified according to the callback's conditions.

---
+++ map.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arrOne, arrTwo) {
+  let str = "";
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] === arrTwo[i]) {
+      str += arrOne[i];
+    }
+  }
+  let compareStr = arrOne.join("");
+  if (compareStr === str) {
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log("The two arrays match!");
+  } else {
+    console.log("The two arrays do not match.");
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results;
+};
+
+//TEST CASES
+
+const fruits = ["Banana", "Kiwi", "Strawberry", "Raspberry", "Blueberry"];
+
+const numbers = [10, 120, 55, 4, 10, 2, 1];
+
+const results1 = map(fruits, word  => word[0]);
+const results2 = map(numbers, num  => num * 2);
+const results3 = map(numbers, num  => num + " is divisible by two: " + (num % 2 === 0));
+
+let assert = assertArraysEqual(results1, ['B', 'K', 'S', 'R', 'B']);
+assert = assertArraysEqual(results1, ['S', 'A', 'B', 'C', 'D']);
+
+console.log(results3);
+console.log(assert);
