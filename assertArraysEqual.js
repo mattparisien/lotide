e83c7c74e4d38c434f712549320ce7ca921f3fe3
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-06 20:08:05 +0000

    Refactoring assertArraysEqual.js by removing the test code, placing it in a seperate file & directory. Further refactoring includes removing a function which was polluting the global scope but was necessary for assertArraysEqual to run properly, instead we simply required that function from assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let str = "";
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] === arrTwo[i]) {
-      str += arrOne[i];
-    }
-  }
-  let compareStr = arrOne.join("");
-  if (compareStr === str) {
-    return true;
-  }
-  return false;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrOne, arrTwo) {
   if (eqArrays(arrOne, arrTwo) === true) {
@@ -20,5 +8,4 @@ const assertArraysEqual = function(arrOne, arrTwo) {
   }
 };
 
-//Test Case: Returning a message using the asserArraysEqual function based on the boolean returned by the eqArrays function
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArraysEqual;
