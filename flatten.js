46b5b3afa390d4c6daec20e7585a9f51b912166e
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-09 15:03:34 +0000

    Refactored flatten.js by removing the test code in the source file and using mocha & chai in a seperate test file. I tested the happy path (array supplied is only one level deep & user actually input something), vs the sad path (in this case would be that the array is various levels deep, and user did not input anything.

--- flatten.js
+++ flatten.js
@@ -1,27 +1,12 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let str = "";
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] === arrTwo[i]) {
-      str += arrOne[i];
-    }
-  }
-  let compareStr = arrOne.join("");
-  if (compareStr === str) {
-    return true;
-  }
-  return false;
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo) === true) {
-    console.log("The two arrays match!");
-  } else {
-    console.log("The two arrays do not match.");
-  }
-};
-
 const flatten = function(array) {
   let flattenedArr = [];
+  
+  array.flat().forEach(val => {
+    if (Array.isArray(val)) {
+      throw new Error;
+    }
+  })
+
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
       for (let j = 0; j < array[i].length; j++) {
@@ -31,15 +16,12 @@ const flatten = function(array) {
       flattenedArr.push(array[i]);
     }
   }
+  // flattenedArr.forEach(val => {
+  //   if (Array.isArray(val)) {
+  //     throw new Error;
+  //   };
+  // })
   return flattenedArr;
 };
-
-// //Test Case 1: Testing the flatten function
-// console.log(flatten([1, 2, [3, 4], 5, [6]]));
-// console.log(flatten([["Coffee"], "tea", ["cookies"]]));
-// console.log(flatten([["Bananas"], "Cherries", ["Lemons", "Avocados"]]));
-
-//Test Case 2: Using the assertArraysEqual to test out 
-// console.log(assertArraysEqual(flatten([1, 2, 3, [4, 5], 6]), [1, 2, 3, 4, 5, 6]));
 
 module.exports = flatten;
