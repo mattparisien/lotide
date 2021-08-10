9208c28d1ab29cabd2200086f80e9eb2f1892d26
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-10 15:24:47 +0000

    First go at implementing tests on letterPositions. So far I've tested for user input, making sure that the input is a string and not any other type of data, and making sure that an error gets thrown if anything but a string is passed in. I've also tested for the correct output (an object), and finally, I've tested to make sure that the returned object contains only one key per character (and that their positions are located in an array, instead of repeating the key).

---
+++ test/letterPositions.test.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const letterPositions = require('../letterPositions');
+
+describe('#letterPositions', () => {
+
+  it('should return an error if the argument is not a string', () => {
+    expect(() => {letterPositions(['foo!'])}).to.throw(Error);
+  });
+
+  it('should return the appropriate letter positions', () => {
+    const actual = letterPositions('matthew');
+    const expected = {
+      m: [0],
+      a: [1],
+      t: [2, 3],
+      h: [4],
+      e: [5],
+      w: [6]
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should only create one key per character, and use an array to store the character\'s locations', () => {
+    const actual = letterPositions('tttt')
+    const expected = {t: [0,1,2,3]}
+    
+    assert.deepEqual(actual, expected);
+  });
+})
