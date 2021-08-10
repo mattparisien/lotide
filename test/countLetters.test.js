6e61c4e1fe1d09a93274328d9256b503a138e743
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-10 18:15:32 +0000

    Implementing three tests for countLetter, I tested for the happy path: in this case the user inputs a string and an object is returned with the correct count of each character. I also made sure to test for errors, like if the user inputs any other data type than a string.

---
+++ test/countLetters.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const countLetters = require('../countLetters');
+
+describe('#countLetters', () => {
+
+  it('should return an error if argument is not a string', () => {
+    expect(() => {countLetters(46354)}).to.throw(Error);
+  });
+
+  it('should still return an object if passed in symbols', () => {
+    const actual = countLetters("%%%%##$$$");
+    const expected = {
+      "%": 4,
+      "#": 2, 
+      "$": 3
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should return an object with all the counts of each letter', () => {
+    const actual = countLetters('matthew');
+    const expected  = {
+      m: 1,
+      a: 1,
+      t: 2,
+      h: 1,
+      e: 1,
+      w: 1
+    };
+
+    assert.deepEqual(actual, expected);
+  })
+})
