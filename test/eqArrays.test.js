423eccb1a70a58c94493dc2000da0dc48f0965d2
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-10 22:44:16 +0000

    Implemented an additional test, checked to see if an error would get thrown if any of the arguments passed in aren't arrays.

--- test/eqArrays.test.js
+++ test/eqArrays.test.js
@@ -13,8 +13,11 @@ describe('#eqArrays', () => {
   it('should return false if the values of the two arrays are the same, but the types are different', () => {
     const actual = eqArrays([1, 2, "3"], [1, 2, 3]); // types are different
     assert.equal(actual, false);
-  })
+  });
 
+  it('should return an error if the data passed in is not an array', () => {
+    expect(() => {eqArrays("hey", [1, 2, 3])}).to.throw(Error);
+  });
 })
 
 
