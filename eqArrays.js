b260a0fc2d4c1628569fd25550201c7aa7869da7
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-10 22:41:31 +0000

    Implemented a conditional statement to throw an error if any of the arguments passed in aren't arrays.

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,9 @@
 const eqArrays = function(arrOne, arrTwo) {
+
+  if (!Array.isArray(arrOne) || !Array.isArray(arrTwo)) {
+    throw new Error('Parameter must be an array.')
+  };
+
   let str = "";
   for (let i = 0; i < arrOne.length; i++) {
     if (arrOne[i] === arrTwo[i]) {
