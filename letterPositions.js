d4759800990e7295f90c2df2fb404da1c63fae17
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-12 16:07:12 +0000

    Removing unnecessary test code.

--- letterPositions.js
+++ letterPositions.js
@@ -1,29 +1,11 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let str = "";
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] === arrTwo[i]) {
-      str += arrOne[i];
-    }
-  }
-  let compareStr = arrOne.join("");
-  if (compareStr === str) {
-    return true;
-  }
-  return false;
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo) === true) {
-    console.log("The two arrays match!");
-  } else {
-    console.log("The two arrays do not match.");
-  }
-};
-
-
 const letterPositions = function(string) {
   const results = {};
   let count = 0;
+
+  if (typeof string !== 'string') {
+    throw new Error('Argument must be of type \'string\'');
+  }
+
   for (i = 0; i < string.length; i++) {
     if (results[string[i]] === undefined) {
       results[string[i]] = new Array;
@@ -34,22 +16,5 @@ const letterPositions = function(string) {
 }
   return results;
 };
-
-
-//TEST CASES
-const sentence = "hello";
-
-const result1 = letterPositions(sentence);
-
-const testObj = {
-  h: [0],
-  e: [1],
-  l: [2, 3],
-  o: [4],
-}
-
-// console.log(assertArraysEqual(result1.e, testObj.e)); //should match
-// console.log(assertArraysEqual(result1.l, testObj.l)); // should match
-// console.log(assertArraysEqual(result1[l], testObj.l)); // should match
 
 module.exports = letterPositions;
