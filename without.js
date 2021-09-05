ca3ca4f35ad089885597fab3512412919b505ad5
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-05 19:05:35 +0000

    Use filter method instead of for loop to fix returned array containing multiple values of values to be returned.

--- without.js
+++ without.js
@@ -1,18 +1,15 @@
 const without = function(source, itemsToRemove) {
 
+  const finalArr = [];
+
   if (!Array.isArray(itemsToRemove)) {
     throw new Error('Second parameter must be an array of strings.')
   };
 
-  let finalArr = [];
-  for (let i = 0; i < source.length; i++) { // loop through each item in first array
-    for (let j = 0; j < itemsToRemove.length; j++) { // for each item in the first array, compare to each item in the second
-      if (source[i] !== itemsToRemove[j]) {
-        finalArr.push(source[i]); //push the values that return true into a new array
-      }
-    }
-  }
-  return finalArr;
-};
+  const result = source.filter(el => {
+    return !itemsToRemove.includes(el)
+  })
+return result;
+}
 
 module.exports = without;
